e5e496278a97c8a191314f462131ae2e2356dd31
Benson Kalahar <bensonk42@gmail.com>
2011-04-29 17:58:26 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Whuf::Application.config.secret_token = '5d36c489d052619fd5dc9c9caeff2e80174dab0b7a725e9b9acd0b499b8316ea387f8fb169e6869d25891f11914364486eb7d74d499ff47ea01f2cb6a73b2cfc'
